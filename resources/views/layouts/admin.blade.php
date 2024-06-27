1eb607c0b971b4a0a380cfa4f4345cd1dc64df2c
DavideTessari <davtex@gmail.com>
2024-06-27 12:53:17 +0200

    feat : modify welcome

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -26,8 +26,8 @@
 <body class="overflow-hidden">
     <div id="app">
 
-        <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-2 shadow">
-            <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="/">BoolPress</a>
+        <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap shadow">
+            <a class="navbar-brand col-md-3 col-lg-2 me-0" href="/">BoolPress</a>
             <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
