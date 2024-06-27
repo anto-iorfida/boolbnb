3d2ca689d302ce467344dbdbf4dcb32802dab5ef
DavideTessari <davtex@gmail.com>
2024-06-27 10:09:22 +0200

    fix apartment

--- app/Http/Controllers/Admin/ApartmentController.php
+++ app/Http/Controllers/Admin/ApartmentController.php
@@ -8,9 +8,6 @@ use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 use App\Models\Apartment;
-
-use Illuminate\Support\Facades\Auth;
-
 use Illuminate\Support\Str;
 
 
