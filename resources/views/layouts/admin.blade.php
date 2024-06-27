78f80962ab0f2d5a2f435a496077788f3f2a9e25
DavideTessari <davtex@gmail.com>
2024-06-27 13:11:37 +0200

    fix

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -28,7 +28,7 @@
     <div id="app">
 
         <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap shadow">
-            <a class="navbar-brand col-md-3 col-lg-2 me-0" href="/">BoolPress</a>
+            <a class="navbar-brand col-md-3 col-lg-2 me-0 text-center" href="/">Bool BnB</a>
             <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
