552bf5ce0e2be9e1c67f228c8e4be8e41b68763a
anto-iorfida <antonioiorfida@hotmail.com>
2024-06-28 12:30:51 +0200

    feat add seed apartment_service

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,10 +1,13 @@
 <?php
 
 namespace Database\Seeders;
+
+use App\Models\Sponsor;
 use Database\Seeders\UsersTableSeeder;
 use Database\Seeders\ApartmentsTableSeeder;
 use Database\Seeders\ServicesTableSeeder;
 use Database\Seeders\MessagesTableSeeder;
+use Database\Seeders\SponsorsTableSeederTableSeeder;
 use Database\Seeders\ViewTableSeeder;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -31,7 +34,8 @@ class DatabaseSeeder extends Seeder
             ApartmentsTableSeeder::class,
             ServicesTableSeeder::class,
             MessagesTableSeeder::class,
-            ViewsTableSeeder::class,
+            // ViewsTableSeeder::class,
+            SponsorsTableSeeder::class,
         ]);
     }
 }
