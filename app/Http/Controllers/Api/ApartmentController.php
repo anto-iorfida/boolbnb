1f3e25f527fae4c9ccbae5ee321feea61ef35e9a
anto-iorfida <antonioiorfida@hotmail.com>
2024-06-28 22:15:11 +0200

    Feat add created apartmentcontroller ina api and created rout in api.php

---
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Apartment;
+use Illuminate\Http\Request;
+
+
+class ApartmentController extends Controller
+{
+    public function index()
+    {
+        $apartments = Apartment::all();
+
+        return response()->json([
+            'success' => true,
+            'result' => $apartments
+        ]);
+    }
+}
