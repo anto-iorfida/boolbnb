e3833c0c5a789cc5ab3da19e7ba65a1cabac2e7a
Mattia Giarrusso <giarrusso.m@yahoo.com>
2024-07-01 12:18:14 +0200

    feat: fix layout + logo

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -33,8 +33,8 @@
 
 <body>
     {{-- |||||||||||||||||||||||||||||||  inizio header ||||||||||||||||||||||||||||||| --}}
-    <header class="sticky-top">
-        <nav class="navbar navbar-light bg-light p-3">
+    <header class="sticky-top ms-bg-header">
+        <nav class="navbar navbar-light p-3">
             <div class="d-flex col-12 col-md-3 col-lg-2 mb-2 mb-lg-0 flex-wrap flex-md-nowrap justify-content-between">
                 <a class="navbar-brand" href="/">
                     <img src="{{ Vite::asset('resources/img/logo-dashboard.png') }}" alt="BoolB&B" class="ms-logo">
@@ -44,8 +44,26 @@
                     <span class="navbar-toggler-icon"></span>
                 </button>
             </div>
+
             <div class="col-12 col-md-5 col-lg-8 d-flex align-items-center justify-content-md-end mt-3 mt-md-0">
-                {{-- \\\\\\\\\\\\\\\\\\\\ fare dropdown nuovo ///////////////////// --}}
+                <div class="btn-group">
+                    <button type="button" class="btn btn-light dropdown-toggle" data-bs-toggle="dropdown"
+                        aria-expanded="false">
+                        Ciao, {{ Auth::user()->name }}
+                    </button>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();"><i
+                                    class="fa-solid fa-arrow-right-from-bracket"></i>
+                                {{ __('Logout') }}
+                            </a>
+                        </li>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                    </ul>
+                </div>
             </div>
         </nav>
     </header>
@@ -58,21 +76,21 @@
                 <div class="position-sticky">
                     <ul class="nav flex-column">
                         <li class="nav-item p-2">
-                            <a class="nav-link {{ Route::currentRouteName() == 'admin.dashboard' ? 'bg-body-secondary text-dark border-start border-primary border-3' : '' }}"
+                            <a class="nav-link {{ Route::currentRouteName() == 'admin.dashboard' ? 'bg-body-secondary text-dark border-start border-primary border-4 rounded' : '' }}"
                                 href="{{ route('admin.dashboard') }}">
                                 <i class="fa-solid fa-house"></i>
                                 <small>Dashboard utente</small>
                             </a>
                         </li>
                         <li class="nav-item p-2">
-                            <a class="nav-link {{ Route::currentRouteName() == 'admin.apartments.index' ? 'bg-body-secondary text-dark border-start border-primary border-3' : '' }}"
+                            <a class="nav-link {{ Route::currentRouteName() == 'admin.apartments.index' ? 'bg-body-secondary text-dark border-start border-primary border-4 rounded' : '' }}"
                                 href="{{ route('admin.apartments.index') }}">
                                 <i class="fa-solid fa-building"></i>
                                 <small>I miei appartamenti</small>
                             </a>
                         </li>
                         <li class="nav-item p-2">
-                            <a class="nav-link {{ Route::currentRouteName() == 'admin.apartments.create' ? 'bg-body-secondary text-dark border-start border-primary border-3' : '' }}"
+                            <a class="nav-link {{ Route::currentRouteName() == 'admin.apartments.create' ? 'bg-body-secondary text-dark border-start border-primary border-4 rounded' : '' }}"
                                 href="{{ route('admin.apartments.create') }}">
                                 <i class="fa-solid fa-plus"></i>
                                 <small>Inserisci appartamento</small>
@@ -84,14 +102,16 @@
             {{-- |||||||||||||||||||||||||||||||  fine sidebar ||||||||||||||||||||||||||||||| --}}
 
             <main class="col-md-9 ml-sm-auto col-lg-10 px-md-4 py-4 bg-white">
-                <nav aria-label="breadcrumb">
-                    <ol class="breadcrumb">
-                        <li class="breadcrumb-item"><a href="/">Home</a></li>
-                        <li class="breadcrumb-item active" aria-current="page">Overview</li>
-                    </ol>
-                </nav>
+                <div>
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="/">Home</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">Overview</li>
+                        </ol>
+                    </nav>
 
-                @yield('content')
+                    @yield('content')
+                </div>
 
                 <footer class="pt-5 d-flex justify-content-between">
                     <span>Copyright © 2024-2025 <a href="/">BoolB&B</a></span>
@@ -135,6 +155,10 @@
 
     body::-webkit-scrollbar-thumb:hover {
         background-color: #0D6EFD;
+    }
+
+    .ms-bg-header {
+        background-color: #0d6efd;
     }
 
     main {
