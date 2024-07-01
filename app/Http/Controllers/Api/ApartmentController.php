2a750b9f7de2b893608924a2e2e6b27f55de3364
anto-iorfida <antonioiorfida@hotmail.com>
2024-07-01 09:55:01 +0200

    add relation

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Apartment;
+use App\Models\Service;
 use Illuminate\Http\Request;
 
 
@@ -11,7 +12,7 @@ class ApartmentController extends Controller
 {
     public function index()
     {
-        $apartments = Apartment::all();
+        $apartments = Apartment::with('services')->paginate(3);
 
         return response()->json([
             'success' => true,
@@ -21,7 +22,7 @@ class ApartmentController extends Controller
 
     public function show($slug)
     {
-        $apartment = Apartment::where('slug', '=', $slug)->first();//aggiungere relazione
+        $apartment = Apartment::where('slug', '=', $slug)->with('services')->first();
 
         if ($apartment) {
             $data = [
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -11,14 +11,16 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Fontawesome 6 cdn -->
-    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css' integrity='sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==' crossorigin='anonymous' referrerpolicy='no-referrer' />
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css'
+        integrity='sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=='
+        crossorigin='anonymous' referrerpolicy='no-referrer' />
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     @yield('scripts')
-    
+
 
 
     <!-- Usando Vite -->
@@ -32,15 +34,18 @@
             <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 text-center" href="/">
                 <img class="ms-logo" src="{{ Vite::asset('resources/img/logo-dashboard.png') }}">
             </a>
-            <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler position-absolute d-md-none collapsed" type="button"
+                data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu"
+                aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             {{-- <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search"> --}}
             <div class="navbar-nav">
                 <div class="nav-item text-nowrap ms-2">
-                    <a class="btn btn-primary rounded-pill ms-btn" href="{{ route('logout') }}" onclick="event.preventDefault();
+                    <a class="btn btn-primary rounded-pill ms-btn" href="{{ route('logout') }}"
+                        onclick="event.preventDefault();
                     document.getElementById('logout-form').submit();">
-                       <i class="fa-solid fa-right-from-bracket"></i> {{ __('Logout') }}
+                        <i class="fa-solid fa-right-from-bracket"></i> {{ __('Logout') }}
                     </a>
                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                         @csrf
@@ -51,25 +56,29 @@
 
         <div class="container-fluid ">
             <div class="row h-100 ">
-                <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-primary navbar-dark sidebar collapse position-fixed h-100">
+                <nav id="sidebarMenu"
+                    class="col-md-3 col-lg-2 d-md-block bg-primary navbar-dark sidebar collapse position-fixed h-100">
                     <div class="position-sticky pt-3">
                         <ul class="nav flex-column">
                             <li class="nav-item">
-                                <a class="nav-link rounded {{ Route::currentRouteName() == 'admin.dashboard' ? 'bg-light text-dark' : '' }}" href="{{ route('admin.dashboard')}}">
+                                <a class="nav-link rounded {{ Route::currentRouteName() == 'admin.dashboard' ? 'bg-light text-dark' : '' }}"
+                                    href="{{ route('admin.dashboard') }}">
                                     <i class="fa-solid fa-gauge-high"></i> Dashboard Utente
                                 </a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link rounded {{ Route::currentRouteName() == 'admin.apartments.index' ? 'bg-light text-dark' : '' }}" href="{{ route('admin.apartments.index')}}">
+                                <a class="nav-link rounded {{ Route::currentRouteName() == 'admin.apartments.index' ? 'bg-light text-dark' : '' }}"
+                                    href="{{ route('admin.apartments.index') }}">
                                     <i class="fa-regular fa-rectangle-list"></i> I tuoi appartamenti
                                 </a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link rounded {{ Route::currentRouteName() == 'admin.apartments.create' ? 'bg-light text-dark' : '' }}" href="{{ route('admin.apartments.create')}}">
+                                <a class="nav-link rounded {{ Route::currentRouteName() == 'admin.apartments.create' ? 'bg-light text-dark' : '' }}"
+                                    href="{{ route('admin.apartments.create') }}">
                                     <i class="fa-solid fa-plus"></i> Inserisci un appartamento
                                 </a>
                             </li>
-                            
+
                         </ul>
 
 
@@ -87,20 +96,21 @@
 
 </html>
 <style>
-
     .ms-bg-primary {
         background-color: white;
     }
-    .nav .nav-item a{
+
+    .nav .nav-item a {
         color: white;
         h
     }
 
-    .nav .nav-item:hover{
+    .nav .nav-item:hover {
         background: white;
         border-radius: 5px;
     }
-    .nav .nav-item:hover a{
+
+    .nav .nav-item:hover a {
         color: black;
         font-size: 18px;
         transition: 0.8s;
@@ -111,5 +121,4 @@
         border-color: #DA26AD;
         color: black;
     }
-
 </style>
