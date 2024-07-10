f895e7c997a70cafc7eba8dca9259a9e88646e0c
anto-iorfida <antonioiorfida@hotmail.com>
2024-07-10 16:35:48 +0200

    fix

--- database/seeders/MessagesTableSeeder.php
+++ database/seeders/MessagesTableSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Factory as Faker;
 use App\Models\Message;
+
 class MessagesTableSeeder extends Seeder
 {
     /**
@@ -13,17 +14,23 @@ class MessagesTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run(Faker $faker)
+    public function run()
     {
-        //
-        for($i = 0; $i < 3 ; $i ++){
-         $newMessage= new Message();
-            $newMessage->email_sender='moliternitest@gmail.com';
-            $newMessage->apartment_id=1;
-            $newMessage->name_lastname='Casimiro Moliterni';
-            $newMessage->body='Prova di test email , lorem ipsum !';
-            $newMessage->save();
+        $faker = Faker::create('it_IT'); // Utilizza la localizzazione italiana per nomi e frasi
 
+        // Ciclo per 3000 appartamenti
+        for ($apartmentId = 1; $apartmentId <= 3000; $apartmentId++) {
+            // Ogni appartamento avrà tra 1 a 3 messaggi
+            $messageCount = rand(1, 3);
+
+            for ($i = 0; $i < $messageCount; $i++) {
+                $newMessage = new Message();
+                $newMessage->email_sender = $faker->unique()->safeEmail;
+                $newMessage->apartment_id = $apartmentId;
+                $newMessage->name_lastname = $faker->name;
+                $newMessage->body = $faker->sentence($nbWords = 10, $variableNbWords = true);
+                $newMessage->save();
+            }
         }
     }
 }
