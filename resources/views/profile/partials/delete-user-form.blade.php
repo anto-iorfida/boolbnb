a7688ee8355fed983b390bb0b12fdf3aaf0b8ba1
Mattia Giarrusso <giarrusso.m@yahoo.com>
2024-07-10 18:50:45 +0200

    Feat: italianizzation profile

--- resources/views/profile/partials/delete-user-form.blade.php
+++ resources/views/profile/partials/delete-user-form.blade.php
@@ -1,17 +1,17 @@
 <section class="space-y-6">
     <header>
         <h2 class="text-lg font-medium text-gray-900">
-            {{ __('Delete Account') }}
+            {{ __('Elimina Account') }}
         </h2>
 
         <p class="mt-1 text-sm text-gray-600">
-            {{ __('Once your account is deleted, all of its resources and data will be permanently deleted. Before deleting your account, please download any data or information that you wish to retain.') }}
+            {{ __("Una volta cancellato l'account, tutte le risorse e i dati saranno eliminati in modo permanente. Prima di eliminare l'account, scaricare tutti i dati e le informazioni che si desidera conservare.") }}
         </p>
     </header>
 
     <!-- Modal trigger button -->
     <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#delete-account">
-        {{__('Delete Account')}}
+        {{__('Elimina Account')}}
     </button>
 
     <!-- Modal Body -->
