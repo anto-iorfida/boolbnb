22ff7109ebf50f19bfae8cf85f79184839336b58
DavideTessari <davtex@gmail.com>
2024-07-10 21:35:14 +0200

    fix color

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-<div class="jumbotron mb-4 bg-light rounded-3" style="height: 50%; overflow: hidden;">
+<div class="jumbotron mb-4 rounded-3" style="overflow: hidden;">
     <div class="container text-center">
         <div class="">
             <img src="https://cdn.discordapp.com/attachments/1253278465843531788/1255827880026574902/Logo_Bool_bb.png?ex=667e8c5a&is=667d3ada&hm=3ec1d15ebb0333dfc7329c9fcd0c6ddf6f1a532447df88dc143ff1e2c3f51084&" class="img-fluid" alt="Logo Bool BnB" style="width:350px">
@@ -12,24 +12,17 @@
     </div>
 </div>
 
-<div class="content">
-    <div class="container text-center">
-        <p style="color: #333;">Bool BnB è dedicato a fornire un'esperienza di prenotazione senza problemi con opzioni diverse che soddisfano le esigenze di ogni viaggiatore. Unisciti alla nostra comunità ed esplora un mondo di soggiorni unici ed esperienze memorabili.</p>
-    </div>
-</div>
 
 <style>
     body, html {
-        height: 100%;
         margin: 0;
         overflow: hidden;
     }
 
     .jumbotron {
-        background-image: url('https://www.example.com/path-to-your-background-image.jpg');
         background-size: cover;
         background-position: center;
-        color: #333; 
+        height:100vh;
     }
     .content {
         padding: 50px 0;
